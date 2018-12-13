0997bd9bb4c8f57b5600ed01f37121afae9cfab5
kwangyo cho <superkkazo@gmail.com>
2018-12-13 13:45:09 +0900

    cloud eureka 1.0.0-SNAPSHOT

---
+++ server/eureka/src/main/java/net/kkazo/cloud/discovery/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+/*
+ * # EurekaApplication.java
+ *
+ * Adcapsule., Software License, Version 1.0
+ * Copyright (c) 2017-2018 ㈜애드캡슐소프트., All rights reserved.
+ *
+ * For more information on this product, please see
+ * https://www.devcapsule.me
+ */
+
+package net.kkazo.discovery.eureka;
+
+/**
+ * TODO: shoud be write javadoc comment
+ *
+ * @author ky.cho
+ */
+public class EurekaApplication {
+
+}
